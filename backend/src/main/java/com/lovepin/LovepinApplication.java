a98a27f9e7c799bbf3623e08fa995d91ca07c4c7
miinspp <qkralstn8332@hanyang.ac.kr>
2026-05-08 04:55:16 +0900

    Initialize Project

---
+++ backend/src/main/java/com/lovepin/LovepinApplication.java
@@ -0,0 +1,13 @@
+package com.lovepin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovepinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LovepinApplication.class, args);
+	}
+
+}
