1f618d5606ea6622780479d80966a293c1469564
miinspp <qkralstn8332@hanyang.ac.kr>
2026-05-09 16:42:54 +0900

    Initial Commit: LovePin 프로젝트 시작

---
+++ src/test/java/com/hanyang/lovepin/LovepinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanyang.lovepin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovepinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
